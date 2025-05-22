1f83067f435d6baf0f7c5efc7bc6a63f34ee2b9b
Arcgel <darkdusty8000@gmail.com>
2025-05-22 14:02:23 +0800

    chore(core): update dependencies in main.ts for compatibility improvements

--- Calendarapp/src/main.ts
+++ Calendarapp/src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VCalendar, {})
 app.use(router)
 
 app.mount('#app')
