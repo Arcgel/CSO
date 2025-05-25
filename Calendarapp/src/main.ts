45119d19bf72d67fdb9041e758a585874181f675
Arcgel <darkdusty8000@gmail.com>
2025-05-25 11:56:35 +0800

    feat: integrate v-calendar and setup calendar functionality
    - Added v-calendar package and integrated it into the project.
    - Updated main.ts to include setupCalendar, Calendar, and DatePicker imports.
    - Configured app to use setupCalendar, createPinia, and router.
    - Modified related Vue components (App.vue, Calendar.vue, LoginView.vue, RegisterView.vue, DashboardView.vue).
    - Updated package.json and package-lock.json to reflect new dependencies.

--- Calendarapp/src/main.ts
+++ Calendarapp/src/main.ts
@@ -6,18 +6,16 @@ import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
-import 'v-calendar/style.css';
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
 
 import App from './App.vue'
 import router from './router'
-
 
 const app = createApp(App)
 
 app.use(setupCalendar, {})
 app.use(createPinia())
 app.use(router)
-
 
 app.mount('#app')
